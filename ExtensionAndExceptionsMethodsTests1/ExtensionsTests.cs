4c21792f143c308ed2827139365855f1d25c8c19
Juemry <65053792+Juemry@users.noreply.github.com>
2021-04-18 04:29:14 -0300

    Updated Work About Methods
    I think its fine, test pending

---
+++ ExtensionAndExceptionsMethodsTests1/ExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExtensionAndExceptionsMethods.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionAndExceptionsMethods.Extensions.Tests
+{
+    [TestClass()]
+    public class ExtensionsTests
+    {
+        [TestMethod()]
+        public void DividirPorTest()
+        {
+
+        }
+    }
+}
---
+++ ExtensionAndExeptionsMethods/ExtensionMethods.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionAndExceptionsMethods.Extensions
+{
+    public static class Extensions
+    {
+
+        public static string DividirPor(this int? dividendo, int? divisor)
+        {
+            int? resultado = null;
+            try
+            {
+                resultado = dividendo / divisor;
+                return $"El resultado de {dividendo}, dividido en {divisor} es {resultado}";
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Mensaje de Error: ");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("La operacion ha finalizado.");
+            }
+            return null;
+        }
+
+        public static string DividirALoNorris(this int? dividendo, int? divisor)
+        {
+            int? resultado = null;
+            try
+            {
+                resultado = dividendo / divisor;
+                return $"El resultado de {dividendo}, dividido en {divisor} es {resultado}";
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Solo Chuck Norris divide por cero!");
+                Console.WriteLine($"El tipo de Error es: {ex.GetType()}");
+                Console.WriteLine($"Mensaje de Error: {(ex.Message)}");
+                Console.WriteLine($"StackTrace: {(ex.StackTrace)}");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"El tipo de Error es: {ex.GetType()}");
+                Console.WriteLine($"Mensaje de Error: {(ex.Message)}");
+                Console.WriteLine($"StackTrace: {(ex.StackTrace)}");
+            }
+            finally
+            {
+                Console.WriteLine("La operacion finaliza cuando Chuck quiere.");
+            }
+            return null;
+        }
+    };
+}
